d3eceeb8b60c691e93866b3a46e3d259d3589865
Kyle Harter <99735799+kylemotion@users.noreply.github.com>
2025-05-02 17:43:25 -0400

    v1 with current ReadME and functionality

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# ae-library-template
-template repository for starting new scripts
+# Batch Render Default Output Module
+
+## About
+
+km-batch-render-default-output-module will export selected compositions in your project panel or active comps in a batch process. Upon finishing rendering, you will see a dialog box showing you the location of your renders along with which comps were exported. Once you click okay on that dialog box your system folder destination will reveal itself. This script will also set the work area to be rendered to the full length of your composition.
+
+## Installation
+
+Download the .jsxbin file from the repo above to use in your version of After Effects.
+The script must be placed in the Scripts directory in the After Effects application folder. That folder will be named “Adobe After Effects [version]” which has been replaced with “AE” below.
+
+Mac OS X
+
+```
+/Applications/AE/Scripts/
+```
+
+Windows
+
+```
+\Program Files\AE\Support Files\Scripts\
+```
+
+If After Effects was running when you installed the script, you’ll need to restart it.
+
+Also, you’ll need to allow After Effects to write files.
+To do that follow this path:
+
+```
+Preferences > General > Check-off “Allow Scripts to Write Files and Access Network” > Close
+Preferences
+```
+
+## Step 1
+
+Select compositions in the project panel or open 1 composition to export current composition.
+
+## Step 2
+
+Launch the script from your favorite script launcher (e.g., Kbar) or launch it inside After Effects by navigating to **File > Scripts > Run Script File**
+
+## Step 3
+
+Choose the folder destination.
+
+## Step 4
+
+Mogrts exported to your destination of choice!
+
+## Known Issues
+
+Final folder only opens at it's root and doesn't open the directory. Will fix on next iteration.
--- km-batch-render-default-output-module/jsx/km-batch-render-default-output-module.jsx
+++ km-batch-render-default-output-module/jsx/km-batch-render-default-output-module.jsx
@@ -14,81 +14,93 @@
 */
 
 
-(function(thisObj){
+(function(){
+
+    //@include "./library/km-functionLibrary.jsx"
 
 
-    var scriptName = "km-batch-render-default-output-module";
-
-    createUI(thisObj)
-
-    function createUI(thisObj){
-        var win = thisObj instanceof Panel
-        ? thisObj
-        : new Window("window", scriptName, undefined, {
-            resizeable: true
-        })
-
-    win.orientation = 'column';
-    win.alignChildren = ["left", "top"];
-
-    var mainGroup = win.add("group", undefined, "Main Group");
-    mainGroup.orientation = 'column';
-
-
-    var applyGroup = mainGroup.add("group", undefined, "Apply Group");
-    applyGroup.orientation = 'row';
-    var applyButton = applyGroup.add("button", undefined, "Apply");
-    applyButton.preferredSize = [-1,30];
-    applyButton.helpTip = "Click: Apply markers to selected layers.\rShift+Click: Apply markers to beginning of a comp."
-
-
-    applyButton.onClick = function(){
     try {
-        app.beginUndoGroup("What script does");
+        app.beginUndoGroup("Batch export renders with default render token");
+        var proj = kmFunctions.getProj();
+        if(!proj) return null
+        var comps = kmFunctions.getComps(proj);
+        if(!comps || comps.compItems.length === 0) return null;
+        var setWorkArea = setCompWorkArea(comps.compItems);
+        var renderQueue = proj.renderQueue;
+        var addToQueue = addToRenderQueue(comps.compItems,renderQueue);
+        var setFilePath = setOutputFilePath(renderQueue);
 
-        var activeComp = app.project.activeItem;
-        var curLayerSel = activeComp.selectedLayers;
-
-        if(!(activeComp && activeComp instanceof CompItem)){
-            alert("Please open a comp first")
-            return
-        }
-
-        // if(!curLayerSel.length){
-        //     alert("Select atleast 1 layer first");
-        //     return
-        // }
-
-        name('#98745d')
-
-      } catch(error) {
+    } catch(error) {
         alert("An error occured on line: " + error.line + "\nError message: " + error.message);
-      } finally {
+    } finally {
         // this always runs no matter what
         app.endUndoGroup()
+    }
+    
+    /// RENDERRRRRR ////
+    app.beginSuppressDialogs();
+    renderQueue.render();
+    app.endSuppressDialogs(false);
+
+    var alertMessage = 
+        'Success!\r\r\
+        Your compositions:\r\r\
+        '+setFilePath.outputFileNames.join("\r")+'\r\r\
+        Have been exported to:\r\r\
+        '+setFilePath.outputFilePath+'\r\r\
+        Click okay to reveal render location\
+        ';
+
+    alert(alertMessage);
+    var destinationFolder = new Folder(setFilePath.outputFilePath);
+    destinationFolder.execute();
+      
+      function addToRenderQueue(comp,queue){
+
+        for(var b = queue.numItems; b>0; b--){
+            var rqItem = queue.item(b);
+            rqItem.remove();
+        }
+
+        for(var i=0; i<comp.length; i++){
+            var renderQueueComp = comp[i];
+            queue.items.add(renderQueueComp);
+        }
+
+        return
+
+      }
+
+      function setCompWorkArea(comp){
+        for(var i = 0; i<comp.length; i++){
+            var item = comp[i];
+            item.workAreaStart = item.duration-item.duration;
+            item.workAreaDuration = item.duration;
+        }
+        return
+      }
+
+      function setOutputFilePath(queue){
+        var outputFileNames = [];
+        for(var i=1; i<=queue.numItems;i++){
+            var item = queue.item(i);
+            var outputModule = item.outputModule(1);
+            outputModuleFile = File.decode(outputModule.file);
+            var outputModuleFileSplit = outputModuleFile.split("/");
+            outputFileNames.push(outputModuleFileSplit[outputModuleFileSplit.length-1]);
+            var outputFilePath = outputModuleFileSplit.slice(0,-2).join("/") + "/";
+            
+        }
+        return {
+            outputFileNames: outputFileNames,
+            outputFilePath: outputFilePath
+        }
       }
       
-    }
-
     
-    function name(hexCode){
-        return alert(hexToRGB(hexCode))
-        
-    }
-
-    win.onResizing = win.onResize = function (){
-        this.layout.resize();
-    };
-
-    if(win instanceof Window){
-        win.center();
-        win.show();
-    } else {
-        win.layout.layout(true);
-        win.layout.resize();
-    }
+      
 
 
-    }
 
-}(this))
+
+}())
