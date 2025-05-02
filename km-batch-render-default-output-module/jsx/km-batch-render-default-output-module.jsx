ad35714a0f8cc690fcdab8c7f405be92b50a7eaa
Kyle Harter <99735799+kylemotion@users.noreply.github.com>
2025-05-02 08:55:25 -0400

    starting new

---
+++ km-batch-render-default-output-module/jsx/km-batch-render-default-output-module.jsx
@@ -0,0 +1,94 @@
+/**
+ * @description a script wiht a UI that will do something really fucking cool in AE
+ * @name km-batch-render-default-output-module.jsx
+ * @author Kyle Harter <kylenmotion@gmail.com>
+ * @version 1.0.0
+ * 
+ * @license This script is provided "as is," without warranty of any kind, expressed or implied. In
+ * no event shall the author be held liable for any damages arising in any way from the use of this
+ * script.
+ * 
+ * 
+ * 
+ * 
+*/
+
+
+(function(thisObj){
+
+
+    var scriptName = "km-batch-render-default-output-module";
+
+    createUI(thisObj)
+
+    function createUI(thisObj){
+        var win = thisObj instanceof Panel
+        ? thisObj
+        : new Window("window", scriptName, undefined, {
+            resizeable: true
+        })
+
+    win.orientation = 'column';
+    win.alignChildren = ["left", "top"];
+
+    var mainGroup = win.add("group", undefined, "Main Group");
+    mainGroup.orientation = 'column';
+
+
+    var applyGroup = mainGroup.add("group", undefined, "Apply Group");
+    applyGroup.orientation = 'row';
+    var applyButton = applyGroup.add("button", undefined, "Apply");
+    applyButton.preferredSize = [-1,30];
+    applyButton.helpTip = "Click: Apply markers to selected layers.\rShift+Click: Apply markers to beginning of a comp."
+
+
+    applyButton.onClick = function(){
+    try {
+        app.beginUndoGroup("What script does");
+
+        var activeComp = app.project.activeItem;
+        var curLayerSel = activeComp.selectedLayers;
+
+        if(!(activeComp && activeComp instanceof CompItem)){
+            alert("Please open a comp first")
+            return
+        }
+
+        // if(!curLayerSel.length){
+        //     alert("Select atleast 1 layer first");
+        //     return
+        // }
+
+        name('#98745d')
+
+      } catch(error) {
+        alert("An error occured on line: " + error.line + "\nError message: " + error.message);
+      } finally {
+        // this always runs no matter what
+        app.endUndoGroup()
+      }
+      
+    }
+
+    
+    function name(hexCode){
+        return alert(hexToRGB(hexCode))
+        
+    }
+
+    win.onResizing = win.onResize = function (){
+        this.layout.resize();
+    };
+
+    if(win instanceof Window){
+        win.center();
+        win.show();
+    } else {
+        win.layout.layout(true);
+        win.layout.resize();
+    }
+
+
+    }
+
+}(this))
